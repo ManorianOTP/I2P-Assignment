cf8d5f6fd1e8309665e6e47938d319697cd1f1ce
614855621 <aaron.scarff@bt.com>
2023-10-10 09:52:44 +0100

    Removing starting comment summarising brief

--- src/store.java
+++ src/store.java
@@ -1,39 +1,3 @@
-/* UNIVERSITY OF SUFFOLK - INTRODUCTION TO PROGRAMMING 
- * Module assignment
- * 
- * Module Lead: Dr. Kakia Chatsiou
- * Last updated 2022-02-25
- * 
- * The assignment starter code consists of 3 files:
- * 
- * a) store.java: this file contains starting code for the inventory
- * management control system. See assignment brief document for 
- * more information on how to build the rest of the application.
- * 
- * b) items.txt: this file contains a list of all items in the inventory
- * with information about their quantities and total price in stock. See 
- * assignment text for more information.
- * 
- * c) transactions.txt: this file contains a list of all the transactions
- * for the day. You will be using it to print out the report of transactions
- * Each time a transaction happens i.e. an item is added or removed, 
- * a record should be stored in transactions.txt
- *  
- *
- * You are asked to work on expanding the starter code so that your Java app can do the following:
- * 
- *  - read and output to the 2 files (transactions.txt, items.txt) as appropriate
- *  - autogenerate a (5-digit) item id ie. 00001 for each new item
- *  - add a new item to the inventory (by appending a line to items.txt) 
- *  - update the quantity of an item already in store (in items.txt)
- *  - remove an item from the inventory (by removing relevant entry in items.txt)
- *  - search for an item in the inventory (items.txt)
- *  - generate and print a daily transaction report (using transactions.txt)
- * 
- * Check out the full assignment brief for more information about the report.
- */
-
-
 import java.util.Scanner;
 
 public class store
