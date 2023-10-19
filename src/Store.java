5b6e85413383e02fdde3dbb896422427d32f2f2c
614855621 <aaron.scarff@bt.com>
2023-10-19 12:51:36 +0100

    Segmented out writing to file and searching from user input, and added a preset search for today's report

--- src/Store.java
+++ src/Store.java
@@ -14,8 +14,7 @@ public class Store
 	static List<CSV> transactions = CSVRead(transactionsFile);
 
 	// open the menu and start taking user input until they put a choice that cancels the session
-	public static void main(String[] args)
-	{
+	public static void main(String[] args)	{
 		DisplayMenu();
 
 		while (sessionActive) {
@@ -37,11 +36,11 @@ public class Store
 			input.next();
 		}
 		switch (userInput) {
-			case 1 -> SearchObjectByProperty(items);
+			case 1 -> SearchPropertyUserInput(items);
 			case 2 -> AddItem();
 			case 3 -> UpdateItemQuantity();
 			case 4 -> RemoveItem();
-			case 5 -> ViewDailyTransactionReport();
+			case 5 -> ViewTransactions();
 			case 6 -> ViewItems();
 			case 7 -> sessionActive = false;
 			default -> System.out.println("Unexpected error occurred, please enter an integer!");
@@ -56,7 +55,7 @@ public class Store
 		System.out.println("2. ADD NEW ITEM");
 		System.out.println("3. UPDATE QUANTITY OF EXISTING ITEM");
 		System.out.println("4. REMOVE ITEM");
-		System.out.println("5. VIEW DAILY TRANSACTION REPORT");
+		System.out.println("5. VIEW TRANSACTIONS");
 		System.out.println("6. VIEW ITEMS IN INVENTORY");
 		System.out.println("---------------------------------");
 		System.out.println("7. Exit\n");
@@ -89,7 +88,7 @@ public class Store
 
 		double totalPrice = unitPrice * qtyInStock;
 
-		List<String> headers = Arrays.asList("id", "description", "unitPrice", "qtyInStock", "totalPrice");
+		List<String> headers = items.get(0).definedFields.stream().toList();
 		List<String> parameters = Arrays.asList(String.valueOf(id),description,String.valueOf(unitPrice),String.valueOf(qtyInStock),String.valueOf(totalPrice));
 
 		System.out.println("\nItem To Be Added:");
@@ -105,19 +104,22 @@ public class Store
 		String confirmation = input.nextLine().trim().toLowerCase();
 
 		if (confirmation.equals("yes") || confirmation.equals("y")) {
-			CSV newItem = new CSV(parameters,headers);
-			try (BufferedWriter bw = new BufferedWriter(new FileWriter(itemsFile, true))) {
-				bw.newLine();  // Add a new line for the new row
-				bw.write(newItem.toCSVFileOutput());  // Write the new row content
-				bw.flush(); // Close the writer to make it available for CSVRead due to CSVRead being in the try block
-				items = CSVRead(itemsFile); // read the files only if the prior write has succeeded
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-
+			WriteToFile(headers, parameters, itemsFile);
 			System.out.println("New Item Added\n");
 		} else {
 			System.out.println("Item not added.\n");
+		}
+	}
+
+	private static void WriteToFile(List<String> headers, List<String> parameters, File file) {
+		CSV newItem = new CSV(parameters, headers);
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+			bw.newLine();  // Add a new line for the new row
+			bw.write(newItem.toCSVFileOutput());  // Write the new row content
+			bw.flush(); // Close the writer to make it available for CSVRead due to CSVRead being in the try block
+			items = CSVRead(file); // read the files only if the prior write has succeeded
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -132,20 +134,33 @@ public class Store
 	}
 
 	// Outputs the transactions list by reading the transactions file, then printing each row to the console
-	private static void ViewDailyTransactionReport() {
-		transactions = CSVRead(transactionsFile);
-		for (CSV csv : transactions) {
-			System.out.println(csv);
+	private static void ViewTransactions() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Do you want to view all the transactions from today, or all time? (today/all): ");
+		String input = scanner.nextLine().trim().toLowerCase();
+
+		while (!input.equals("today") && !input.equals("all")) {
+			System.out.println("Invalid input. Please enter 'today' or 'all'.");
+			input = scanner.nextLine().trim().toLowerCase();
 		}
+
+		if (input.equals("today")) {
+			SearchPropertyByNameValue(transactions,"date", "19/10/2023");
+		} else {
+			for (CSV csv : transactions) {
+				System.out.println(csv);
+			}
+		}
+
 	}
 
 	// Provide a list of CSVs, practically either items.txt or transactions.txt but could be any
 	// Then provide a property you want to search for. This input should be validated against the headers list if
 	// possible, or some other validation prior to this function. Then provide a value to search that property for.
 	// Should then return the indexes for all object's with a property that matches the specified value
-	private static void SearchObjectByProperty(List<CSV> csvs) {
+	private static void SearchPropertyUserInput(List<CSV> csvs) {
 		Scanner input = new Scanner(System.in);
-		// Change this to be out of a preset list of choices, just for testing atm
 
 		System.out.println("Please input a property name from the following list");
 		System.out.println(csvs.get(0).definedFields);
@@ -157,10 +172,14 @@ public class Store
 		}
 		System.out.println("Please input a value");
 		String value = input.nextLine();
+		SearchPropertyByNameValue(csvs, propertyName, value);
+	}
+
+	private static void SearchPropertyByNameValue(List<CSV> csvs, String propertyName, String value) {
 		for (CSV csv: csvs) {
 			// Pattern.quote to treat the user input as not regex, with 0+ wildcards preceding and following
 			if (csv.GetPropertyByName(propertyName).toString().matches((".*" + Pattern.quote(value) + ".*"))) {
-				System.out.println(csv.GetPropertyByName(propertyName));
+				System.out.println(csv);
 			}
 		}
 	}
